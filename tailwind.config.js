891ff0539eddd1a28d39c77bd35bf057d86a279a
Caleb Lingle <caleb.lingle1@gmail.com>
2025-04-27 16:48:17 -0400

    Corrected some information and fixed nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#1e4220', // Forest green
-        'secondary': '#1991b8', // Blue
+        'primary': '#1991b8', // Blue
+        'secondary': '#1e4220', // Forest Green
         'accent': '#f7d064', // Gold/Yellow
         'dark': '#111827', // Dark background
         'light': '#f9fafb', // Light background
